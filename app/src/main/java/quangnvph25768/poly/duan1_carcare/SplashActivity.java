a0c8c76735e36ed9b3b1ce2ecb2fa0c02aa5ca47
HP <quangnv572003@gmail.com>
2022-11-11 07:58:34 +0700

    Trưởng nhóm tạo dự án

---
+++ app/src/main/java/quangnvph25768/poly/duan1_carcare/SplashActivity.java
@@ -0,0 +1,14 @@
+package quangnvph25768.poly.duan1_carcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
