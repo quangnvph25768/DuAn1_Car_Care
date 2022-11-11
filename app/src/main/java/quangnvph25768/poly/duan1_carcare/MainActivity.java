45ff6fe30860762f242247b947fc70d1190cb28b
HP <quangnv572003@gmail.com>
2022-11-11 07:49:29 +0700

    Trưởng nhóm tạo dự án

---
+++ app/src/main/java/quangnvph25768/poly/duan1_carcare/MainActivity.java
@@ -0,0 +1,14 @@
+package quangnvph25768.poly.duan1_carcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
